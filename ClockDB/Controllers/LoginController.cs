b5f64e3d109de34eeee847e66e251f6f2b24b36b
balagtasblessie <blessie.balagtas@pointwest.com.ph>
2024-04-24 12:09:46 +0800

    first commit

---
+++ ClockDB/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ClockDB.Data;
+using ClockDB.Models;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace ClockDB.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ClockDBContext _context;
+
+        public LoginController(ClockDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] userLogin request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.UserName) || string.IsNullOrEmpty(request.Password))
+            {
+                return BadRequest("Invalid credentials");
+            }
+
+            var user = _context.UserTable
+                .Where(u => u.UserName == request.UserName && u.Password == request.Password)
+                .Join(_context.Role, u => u.id, r => r.id, (u, r) => new { u.FullName, r.roleName })
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            return Ok(user);
+        }
+    }
+}
